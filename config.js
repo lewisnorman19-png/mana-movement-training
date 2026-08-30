813c6e597f0a9f912024ab16c9865986496f1ee0
lewisnorman19-png <lewisnorman19@gmail.com>
2026-08-30 16:25:06 +1000

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,8 @@ window.MANA_CONFIG = {
   supabaseUrl: "https://gdlhmzwntiyoeqnyghro.supabase.co",
   supabaseAnonKey: "sb_publishable_bDaXTP62EJ9WplnhoHGIWQ_dppzV7z1"
 };
+(function(){
+const s=document.createElement('script');
+s.src='v5_5_strength_sets.js?v=5.5.0';
+document.head.appendChild(s);
+})();
