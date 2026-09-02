3bb5237c7f72b3d1a12ba4af63813271a28a4c61
lewisnorman19-png <lewisnorman19@gmail.com>
2026-09-03 08:12:55 +1000

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,11 @@ window.MANA_CONFIG = {
   supabaseAnonKey: "sb_publishable_bDaXTP62EJ9WplnhoHGIWQ_dppzV7z1"
 };
 (function(){
-const s=document.createElement('script');
-s.src='v5_5_strength_sets.js?v=5.5.0';
-document.head.appendChild(s);
+  const s=document.createElement('script');
+  s.src='v5_5_strength_sets.js?v=5.5.0';
+  document.head.appendChild(s);
+
+  const mobile=document.createElement('script');
+  mobile.src='v5_6_mobile_scale.js?v=5.6.0';
+  document.head.appendChild(mobile);
 })();
